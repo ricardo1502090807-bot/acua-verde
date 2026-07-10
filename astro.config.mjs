c7d12f1c122ca5eab4b95abdb3ed331193d5941c
Rick <ricardo1502090807@gmail.com>
2026-07-10 18:20:52 -0500

    configuracion de astroconfi con netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  vite:{
-      plugins: [tailwindcss()]
+  output: 'server', // Necesario para que funcione el adaptador
+  adapter: netlify(), // Aquí configuramos el adaptador
+  vite: {
+    plugins: [tailwindcss()],
   },
   integrations: [icon()],
-  image:{
-    domains:['spotcheese.s6-tastewp.com'] 
-  }
+  image: {
+    domains: ['spotcheese.s6-tastewp.com'],
+  },
 });
