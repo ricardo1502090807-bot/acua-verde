72528e3b4e2d676091e2d2c49f1a41fe2523c031
Rick <ricardo1502090807@gmail.com>
2026-07-10 20:42:40 -0500

    fix: configurar alias de forma absoluta para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import netlify from '@astrojs/netlify/functions';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', 
-  adapter: netlify(), 
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': '/src',
+        // Usamos path.resolve para garantizar la ruta absoluta
+        '@': path.resolve(__dirname, './src'),
       },
     },
   },
